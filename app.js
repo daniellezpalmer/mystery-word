368d5a637b413ee5abd25b7827630c8ceae61124
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-29 16:50:53 -0500

    Added session, pushed to session, passed to mustache and rendered

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
-const expressSession = require('express-session')
+const session = require('express-session')
 const expressValidator = require('express-validator');
 const app = express();
 
-const words = require('./words')
-const wordsArray = words;
+const wordsArray = require('./words')
 console.log(wordsArray)
 
 let randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 console.log(randomWord)
 
-let blanks = randomWord.replace(/[a-z]/gi, '_')
+let blanks = randomWord.replace(/[a-z]/gi, '_ ')
 console.log(blanks);
 
-let alphaGuesses = blanks.replace(randomWord)
-console.log(alphaGuesses);
+// guessLetter(){
+//
+// }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.get('/', function(req, res) {
-  res.render('game', {hint: blanks});
+  req.session.blanks = blanks;
+  res.render('game', req.session);
+})
+
+app.post('/', function(req, res){
+  console.log(req.body.guesses);
+  if (!req.session.letters){
+    req.session.letters = [];
+  }
+  req.session.letters.push(req.body.guesses);
+  res.redirect('/');
 })
 
 app.listen(3000, function() {
